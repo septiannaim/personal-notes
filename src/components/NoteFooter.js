4344265087bcc7aaec11ee2514b0d6a1a3925126
septiannaim <naimplung28@gmail.com>
2024-12-07 22:31:44 +0700

    first commit

---
+++ src/components/NoteFooter.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NoteFooter() {
+  return (
+    <div className="note-app__footer">
+      <p>Copyright &#169; 2024, Septiannaim</p>
+    </div>
+  );
+}
+
+export default NoteFooter;
